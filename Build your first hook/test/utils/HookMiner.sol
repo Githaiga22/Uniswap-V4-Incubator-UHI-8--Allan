18717c79488aef9aff6c942485ed4503168960d7
Githaiga22 <allangithaiga5@gmail.com>
2026-01-28 10:59:31 +0300

    feat(HookMiner.sol): Part 2: Add main implementation
    [Commit 2/5 for HookMiner.sol]

--- Build your first hook/test/utils/HookMiner.sol
+++ Build your first hook/test/utils/HookMiner.sol
@@ -7,3 +7,12 @@ pragma solidity ^0.8.24;
  * @author Allan Robinson
  * @notice Utility for mining valid hook addresses via CREATE2
  * @dev Created: January 27, 2026
+ */
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+
+library HookMiner {
+    // mask to slice out the bottom 14 bits of the address
+    uint160 constant FLAG_MASK = 0x3FFF;
+
+    // Maximum number of iterations to find a salt, avoid infinite loops
+    uint256 constant MAX_LOOP = 100_000;
