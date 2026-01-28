b42c8ccbe36af398b285dfedbca69b83d14850e0
Githaiga22 <allangithaiga5@gmail.com>
2026-01-28 10:59:31 +0300

    feat(MyFirstHook.t.sol): Part 2: Add main implementation
    [Commit 2/5 for MyFirstHook.t.sol]

--- Build your first hook/test/MyFirstHook.t.sol
+++ Build your first hook/test/MyFirstHook.t.sol
@@ -16,3 +16,21 @@ import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
 import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
 import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/src/types/PoolId.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/src/types/Currency.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+import {ModifyLiquidityParams} from "@uniswap/v4-core/src/types/PoolOperation.sol";
+import {HookMiner} from "../test/utils/HookMiner.sol";
+
+contract MyFirstHookTest is Test, Deployers {
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+
+    MyFirstHook hook;
+
+    function setUp() public {
+        // Deploy v4 core contracts
+        deployFreshManagerAndRouters();
+
+        // Mine a salt that will produce a hook address with the correct flags
+        uint160 flags = uint160(
+            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
