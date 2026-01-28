ff3b4363e0a1b3588626f9b140f1ac5dc6258ae1
Githaiga22 <allangithaiga5@gmail.com>
2026-01-28 10:59:31 +0300

    feat(DeployHook.s.sol): Part 1: Initialize with core structure
    [Commit 1/5 for DeployHook.s.sol]

---
+++ Build your first hook/script/DeployHook.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+/**
+ * @title DeployHook
+ * @author Allan Robinson
+ * @notice Deployment script for Uniswap V4 hooks
+ * @dev Created: January 27, 2026
