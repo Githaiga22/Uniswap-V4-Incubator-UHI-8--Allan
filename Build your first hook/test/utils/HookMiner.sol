3fee3176af3f567332f75e3b90e77e0f23e0daa4
Githaiga22 <allangithaiga5@gmail.com>
2026-01-28 10:59:31 +0300

    feat(HookMiner.sol): Part 1: Initialize with core structure
    [Commit 1/5 for HookMiner.sol]

---
+++ Build your first hook/test/utils/HookMiner.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+
+/**
+ * @title HookMiner
+ * @author Allan Robinson
+ * @notice Utility for mining valid hook addresses via CREATE2
+ * @dev Created: January 27, 2026
