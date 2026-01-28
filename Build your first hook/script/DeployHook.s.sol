7a3b5d2c4e36e2a999cf0065e0d8071e2a93ff7b
Githaiga22 <allangithaiga5@gmail.com>
2026-01-28 10:59:31 +0300

    feat(DeployHook.s.sol): Part 2: Add main implementation
    [Commit 2/5 for DeployHook.s.sol]

--- Build your first hook/script/DeployHook.s.sol
+++ Build your first hook/script/DeployHook.s.sol
@@ -6,3 +6,11 @@ pragma solidity ^0.8.24;
  * @author Allan Robinson
  * @notice Deployment script for Uniswap V4 hooks
  * @dev Created: January 27, 2026
+ */
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {MyFirstHook} from "../src/examples/MyFirstHook.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+
